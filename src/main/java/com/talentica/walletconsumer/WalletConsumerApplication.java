0f0b751c636e7c52a261fc9afa3da9d76e22fb16
pritish <pritish.desai@talentica.com>
2024-01-10 14:42:06 +0530

    Wallet Consumer > Initial Commit

---
+++ src/main/java/com/talentica/walletconsumer/WalletConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.talentica.walletconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talentica/walletconsumer/WalletConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentica.walletconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
