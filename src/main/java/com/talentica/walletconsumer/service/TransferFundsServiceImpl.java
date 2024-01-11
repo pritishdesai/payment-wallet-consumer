ef3c4de45d053031b802136b25736268fa769eee
pritish <pritish.desai@talentica.com>
2024-01-11 12:06:25 +0530

    Wallet Consumer > transfer-funds-consumer

--- src/main/java/com/talentica/walletconsumer/service/TransferFundsServiceImpl.java
+++ src/main/java/com/talentica/walletconsumer/service/TransferFundsServiceImpl.java
@@ -8,8 +8,12 @@ public class TransferFundsServiceImpl implements TransferFundsService{
     @Override
     public void transferFunds(TransferRequestDto transferRequestDto) {
         //1.Get Balance for both sender and receiver
+
+
+
         //2. Deduct the amount specified from the sender balance & Add the amount in receiver balance.
         //3. Save both the wallet objects in the DB.
+
 
 
 
