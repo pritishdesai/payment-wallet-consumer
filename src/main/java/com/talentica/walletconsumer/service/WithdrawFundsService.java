337641a8811f6206b30235ee8a18a9962333c5cd
pritish <pritish.desai@talentica.com>
2024-01-17 18:51:41 +0530

    Wallet Consumer > withdraw-funds-consumer
    Without Stripe Integration

--- src/main/java/com/talentica/walletconsumer/service/WithdrawFundsService.java
+++ src/main/java/com/talentica/walletconsumer/service/WithdrawFundsService.java
@@ -1,4 +1,8 @@
 package com.talentica.walletconsumer.service;
 
+import com.talentica.walletconsumer.dto.AddWithdrawFundsDto;
+
 public interface WithdrawFundsService {
+
+    public void withdrawFunds(AddWithdrawFundsDto addWithdrawFundsDto);
 }
